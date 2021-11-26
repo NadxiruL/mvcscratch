a66cff6fa31f8a5b91d9e3378fcd50d2c1330c8e
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2021-11-26 10:19:30 +0800

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/home' , function () {
+
+    return 'Hello World';
+
+});
+
+$app->router->get('/contact' , function () {
+
+    return 'Contact';
+
+});
+
+
+
+$app->run();
